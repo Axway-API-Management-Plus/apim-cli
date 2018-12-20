aac11b43b13dd304ed04d866f6a2ce4231d534fc
rathnapandi <rathnapandi.n@gmail.com>
2018-12-19 20:45:17 -0700

    Support for outbound authentication - Unit test

---
+++ src/test/java/com/axway/apim/test/security/OutboundBasicAuthTest.java
@@ -0,0 +1,45 @@
+package com.axway.apim.test.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.testng.annotations.Test;
+
+import com.axway.apim.test.SwaggerImportTestAction;
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.dsl.testng.TestNGCitrusTestDesigner;
+import com.consol.citrus.functions.core.RandomNumberFunction;
+import com.consol.citrus.message.MessageType;
+
+@Test(testName = "OutboundBasicAuthTest")
+public class OutboundBasicAuthTest extends TestNGCitrusTestDesigner {
+
+	@Autowired
+	private SwaggerImportTestAction swaggerImport;
+
+	@CitrusTest(name = "OutboundBasicAuthTest")
+	public void setupDevOrgTest() {
+		description("Verify no error appears, if Authentication is not configured! Must be Passthrough");
+
+		variable("apiNumber", RandomNumberFunction.getRandomNumber(3, true));
+		variable("apiPath", "/no-authn-test-${apiNumber}");
+		variable("apiName", "No AuthN Test ${apiNumber}");
+		variable("status", "unpublished");
+
+		echo("####### Importing API: '${apiName}' on path: '${apiPath}' with following settings: #######");
+		createVariable("swaggerFile", "/com/axway/apim/test/files/security/petstore.json");
+		createVariable("configFile", "/com/axway/apim/test/files/security/outbound-basic.json");
+		createVariable("expectedReturnCode", "0");
+		action(swaggerImport);
+
+		echo("####### Validate API: '${apiName}' on path: '${apiPath}' with Passthrough-Security #######");
+		http().client("apiManager").send().get("/proxies").name("api").header("Content-Type", "application/json");
+
+		http().client("apiManager").receive().response(HttpStatus.OK).messageType(MessageType.JSON)
+				.validate("$.[?(@.path=='${apiPath}')].name", "${apiName}")
+				.validate("$.[?(@.path=='${apiPath}')].state", "unpublished")
+				.validate("$.[?(@.path=='${apiPath}')].authenticationProfiles[0].name	", "test HTTP Basic")
+				.extractFromPayload("$.[?(@.path=='${apiPath}')].id", "apiId");
+	}
+
+
+}
