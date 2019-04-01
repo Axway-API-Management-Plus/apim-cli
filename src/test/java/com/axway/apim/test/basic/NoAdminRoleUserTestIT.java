f8f85ad187425233cbd1daaa5052ecfe00ad63a1
Chris Wiechmann <cwiechmann@axway.com>
2019-04-01 09:35:09 +0200

    To avoid confusion as in #32, now validating user has admin-role.
    If a user is provided without admin-role the following error message is
    logged:
    Given user: 'anna' has no admin-role. This tool needs admin role
    permissions.

---
+++ src/test/java/com/axway/apim/test/basic/NoAdminRoleUserTestIT.java
@@ -0,0 +1,34 @@
+package com.axway.apim.test.basic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.Test;
+
+import com.axway.apim.test.SwaggerImportTestAction;
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.dsl.testng.TestNGCitrusTestDesigner;
+import com.consol.citrus.functions.core.RandomNumberFunction;
+
+@Test(testName="NoAdminRoleUserTestIT")
+public class NoAdminRoleUserTestIT extends TestNGCitrusTestDesigner {
+	
+	@Autowired
+	private SwaggerImportTestAction swaggerImport;
+	
+	@CitrusTest(name = "NoAdminRoleUserTestIT")
+	public void setupDevOrgTest() {
+		description("Is a non-admin role user is used, the tool must fail with a dedicated return code.");
+		
+		variable("apiNumber", RandomNumberFunction.getRandomNumber(3, true));
+		variable("apiPath", "/my-no-change-${apiNumber}");
+		variable("apiName", "No-Change-${apiNumber}");
+
+		echo("####### Calling the tool with a Non-Admin-User. #######");
+		createVariable("swaggerFile", "/com/axway/apim/test/files/basic/petstore.json");
+		createVariable("configFile", "/com/axway/apim/test/files/basic/1_no-change-config.json");
+		createVariable("expectedReturnCode", "17");
+		createVariable("apiManagerUser", "anna"); // This is an org-admin user
+		createVariable("apiManagerPass", "anna");
+		action(swaggerImport);
+	}
+
+}
