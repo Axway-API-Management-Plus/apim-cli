b295a1b9dd794824345c13f5e1983a45445d671e
Chris Wiechmann <cwiechmann@axway.com>
2019-04-09 19:06:31 +0200

    Another fix to test with 7.7

--- build/pull_apim_docker_image.sh
+++ build/pull_apim_docker_image.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ -f $CACHE_FILE_APIM ]
+if [ -f $CACHE_FILE_APIM_7_7 ]
 then
-	echo "Loading docker image: $APIM_DOCKER_IMAGE from $CACHE_FILE_APIM"
-	gunzip -c $CACHE_FILE_APIM | docker load
+	echo "Loading docker image: $APIM_DOCKER_IMAGE from $CACHE_FILE_APIM_7_7"
+	gunzip -c $CACHE_FILE_APIM_7_7 | docker load
 else 
 	echo "Pulling APIM docker from registry, this will take a while"
-	docker pull $APIM_DOCKER_IMAGE
+	docker pull $APIM_DOCKER_IMAGE_7_7
 fi
 
 
