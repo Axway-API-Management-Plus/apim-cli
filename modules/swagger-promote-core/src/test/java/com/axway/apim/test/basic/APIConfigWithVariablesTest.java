8a6250aa1fac7855092ede086ccd5985f31fcb15
Chris Wiechmann <cwiechmann@axway.com>
2019-07-08 14:10:46 +0200

    Try to understand why UnitTests are not working at Travis.

--- modules/swagger-promote-core/src/test/java/com/axway/apim/test/basic/APIConfigWithVariablesTest.java
+++ modules/swagger-promote-core/src/test/java/com/axway/apim/test/basic/APIConfigWithVariablesTest.java
@@ -4,6 +4,8 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,53 +16,71 @@ import com.axway.apim.swagger.APIImportConfigAdapter;
 import com.axway.apim.swagger.api.state.DesiredAPI;
 
 public class APIConfigWithVariablesTest {
+
+	private static Logger LOG = LoggerFactory.getLogger(APIConfigWithVariablesTest.class);
 	
 	@Test
 	public void withoutStage() throws AppException, ParseException {
-		EnvironmentProperties props = new EnvironmentProperties(null);
-		CommandLine cmd = new DefaultParser().parse(new Options(), new String[]{});
-		new CommandParameters(cmd, null, props);
-		String testConfig = this.getClass().getResource("/com/axway/apim/test/files/basic/api-config-with-variables.json").getFile();
-		
-		APIImportConfigAdapter adapter = new APIImportConfigAdapter(testConfig, null, "notRelavantForThis Test", false);
-		DesiredAPI apiConfig = (DesiredAPI)adapter.getApiConfig();
-		Assert.assertEquals(apiConfig.getBackendBasepath(), "resolvedToSomething");
+		try {
+			EnvironmentProperties props = new EnvironmentProperties(null);
+			CommandLine cmd = new DefaultParser().parse(new Options(), new String[]{});
+			new CommandParameters(cmd, null, props);
+			String testConfig = this.getClass().getResource("/com/axway/apim/test/files/basic/api-config-with-variables.json").getFile();
+			
+			APIImportConfigAdapter adapter = new APIImportConfigAdapter(testConfig, null, "notRelavantForThis Test", false);
+			DesiredAPI apiConfig = (DesiredAPI)adapter.getApiConfig();
+			Assert.assertEquals(apiConfig.getBackendBasepath(), "resolvedToSomething");
+		} catch (Exception e) {
+			LOG.error("Error running test: withoutStage", e);
+		}
 	}
 	
 	@Test
 	public void withStage() throws AppException, ParseException {
-		EnvironmentProperties props = new EnvironmentProperties("variabletest");
-		CommandLine cmd = new DefaultParser().parse(new Options(), new String[]{});
-		new CommandParameters(cmd, null, props);
-		String testConfig = this.getClass().getResource("/com/axway/apim/test/files/basic/api-config-with-variables.json").getFile();
-		
-		APIImportConfigAdapter adapter = new APIImportConfigAdapter(testConfig, null, "notRelavantForThis Test", false);
-		DesiredAPI apiConfig = (DesiredAPI)adapter.getApiConfig();
-		Assert.assertEquals(apiConfig.getBackendBasepath(), "resolvedToSomethingElse");
+		try {
+			EnvironmentProperties props = new EnvironmentProperties("variabletest");
+			CommandLine cmd = new DefaultParser().parse(new Options(), new String[]{});
+			new CommandParameters(cmd, null, props);
+			String testConfig = this.getClass().getResource("/com/axway/apim/test/files/basic/api-config-with-variables.json").getFile();
+			
+			APIImportConfigAdapter adapter = new APIImportConfigAdapter(testConfig, null, "notRelavantForThis Test", false);
+			DesiredAPI apiConfig = (DesiredAPI)adapter.getApiConfig();
+			Assert.assertEquals(apiConfig.getBackendBasepath(), "resolvedToSomethingElse");
+		} catch (Exception e) {
+			LOG.error("Error running test: withStage", e);
+		}
 	}
 	
 	@Test
 	public void usingOSEnvVariable() throws AppException, ParseException {
-		EnvironmentProperties props = new EnvironmentProperties(null);
-		CommandLine cmd = new DefaultParser().parse(new Options(), new String[]{});
-		new CommandParameters(cmd, null, props);
-		String testConfig = this.getClass().getResource("/com/axway/apim/test/files/basic/api-config-with-variables.json").getFile();
-		
-		APIImportConfigAdapter adapter = new APIImportConfigAdapter(testConfig, null, "notRelavantForThis Test", false);
-		DesiredAPI apiConfig = (DesiredAPI)adapter.getApiConfig();
-		String osArch = System.getProperty("os.arch");
-		Assert.assertEquals(apiConfig.getOrganization(), "API Development "+osArch);
+		try {
+			EnvironmentProperties props = new EnvironmentProperties(null);
+			CommandLine cmd = new DefaultParser().parse(new Options(), new String[]{});
+			new CommandParameters(cmd, null, props);
+			String testConfig = this.getClass().getResource("/com/axway/apim/test/files/basic/api-config-with-variables.json").getFile();
+			
+			APIImportConfigAdapter adapter = new APIImportConfigAdapter(testConfig, null, "notRelavantForThis Test", false);
+			DesiredAPI apiConfig = (DesiredAPI)adapter.getApiConfig();
+			String osArch = System.getProperty("os.arch");
+			Assert.assertEquals(apiConfig.getOrganization(), "API Development "+osArch);
+		} catch (Exception e) {
+			LOG.error("Error running test: usingOSEnvVariable", e);
+		}
 	}
 	
 	@Test
 	public void notDeclaredVariable() throws AppException, ParseException {
-		EnvironmentProperties props = new EnvironmentProperties(null);
-		CommandLine cmd = new DefaultParser().parse(new Options(), new String[]{});
-		new CommandParameters(cmd, null, props);
-		String testConfig = this.getClass().getResource("/com/axway/apim/test/files/basic/api-config-with-variables.json").getFile();
-		
-		APIImportConfigAdapter adapter = new APIImportConfigAdapter(testConfig, null, "notRelavantForThis Test", false);
-		DesiredAPI apiConfig = (DesiredAPI)adapter.getApiConfig();
-		Assert.assertEquals(apiConfig.getVersion(), "${notDeclared}");
+		try {
+			EnvironmentProperties props = new EnvironmentProperties(null);
+			CommandLine cmd = new DefaultParser().parse(new Options(), new String[]{});
+			new CommandParameters(cmd, null, props);
+			String testConfig = this.getClass().getResource("/com/axway/apim/test/files/basic/api-config-with-variables.json").getFile();
+			
+			APIImportConfigAdapter adapter = new APIImportConfigAdapter(testConfig, null, "notRelavantForThis Test", false);
+			DesiredAPI apiConfig = (DesiredAPI)adapter.getApiConfig();
+			Assert.assertEquals(apiConfig.getVersion(), "${notDeclared}");
+		} catch (Exception e) {
+			LOG.error("Error running test: notDeclaredVariable", e);
+		}
 	}
 }
