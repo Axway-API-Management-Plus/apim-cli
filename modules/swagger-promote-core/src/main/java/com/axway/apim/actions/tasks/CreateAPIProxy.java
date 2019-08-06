b3bef66ce42ef6ba37e2c2d981a77d7fa3f1b40b
Chris Wiechmann <cwiechmann@axway.com>
2019-08-06 19:04:45 +0200

    Print out the last request on APIProxyCreation in error case.

--- modules/swagger-promote-core/src/main/java/com/axway/apim/actions/tasks/CreateAPIProxy.java
+++ modules/swagger-promote-core/src/main/java/com/axway/apim/actions/tasks/CreateAPIProxy.java
@@ -6,6 +6,7 @@ import java.net.URI;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.util.EntityUtils;
@@ -51,8 +52,10 @@ public class CreateAPIProxy extends AbstractAPIMTask implements IResponseParser 
 		JsonNode jsonNode = null;
 		try {
 			if(httpResponse.getStatusLine().getStatusCode()!=201) {
+				Object lastRequest = Transaction.getInstance().get("lastRequest");
 				ErrorState.getInstance().setError("Error creating API-Proxy. "
 						+ "Unexpected response from API-Manager: " + httpResponse.getStatusLine() + " " + EntityUtils.toString(httpResponse.getEntity()) + ". "
+								+ "Last request: '"+EntityUtils.toString(((HttpEntityEnclosingRequestBase)lastRequest).getEntity())+"'"
 								+ "Please check the API-Manager traces.", ErrorCode.CANT_CREATE_API_PROXY, false);
 				throw new AppException("Error creating API-Proxy", ErrorCode.CANT_CREATE_API_PROXY);
 			}
