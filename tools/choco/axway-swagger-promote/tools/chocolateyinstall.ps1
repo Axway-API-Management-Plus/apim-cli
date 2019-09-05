7d22d8d716c16b4eb54662ad82ee9a5c3384985f
Chris Wiechmann <cwiechmann@axway.com>
2019-09-05 16:35:39 +0200

    [skip ci] Using the Choco-Package-version to reference the scripts

--- tools/choco/axway-swagger-promote/tools/chocolateyinstall.ps1
+++ tools/choco/axway-swagger-promote/tools/chocolateyinstall.ps1
@@ -13,11 +13,11 @@ $packageArgs = @{
 }
 
 Get-ChocolateyUnzip "$fileLocation" $toolsDir
-Install-BinFile -Name "api-import" -Path "$toolsDir\swagger-promote-1.6.2-SNAPSHOT\scripts\api-import.bat" -Command "choco"
-Install-BinFile -Name "api-export" -Path "$toolsDir\swagger-promote-1.6.2-SNAPSHOT\scripts\api-export.bat" -Command "choco"
+Install-BinFile -Name "api-import" -Path "$toolsDir\swagger-promote-$env:chocolateyPackageVersion\scripts\api-import.bat" -Command "choco"
+Install-BinFile -Name "api-export" -Path "$toolsDir\swagger-promote-$env:chocolateyPackageVersion\scripts\api-export.bat" -Command "choco"
 
 Write-Output "------------------------------------------------------------------------"
-Write-Output "Axway Swagger-Promote has been installed."
+Write-Output "Axway Swagger-Promote: $env:chocolateyPackageVersion has been installed."
 Write-Output ""
 Write-Output "Execute: 'api-import' to import new APIs"
 Write-Output "Execute: 'api-export' to export existing APIs"
