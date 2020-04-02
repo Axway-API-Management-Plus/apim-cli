0dd79eb73e6b2c5bd8bdccddbfb2420647eaa48d
Chris Wiechmann <cwiechmann@axway.com>
2020-04-02 20:02:29 +0200

    #254 Added some documentation

--- modules/swagger-promote-core/src/test/java/com/axway/apim/test/lib/ErrorCodeMapperTest.java
+++ modules/swagger-promote-core/src/test/java/com/axway/apim/test/lib/ErrorCodeMapperTest.java
@@ -36,4 +36,22 @@ public class ErrorCodeMapperTest {
 		ErrorCode code = mapper.getMapedErrorCode(ErrorCode.API_MANAGER_COMMUNICATION);
 		Assert.assertEquals(code, ErrorCode.API_MANAGER_COMMUNICATION);
 	}
+	
+	@Test
+	public void testMapper4() throws AppException, ParseException {
+		ErrorCodeMapper mapper = new ErrorCodeMapper();
+		mapper.setMapConfiguration("177:0");
+
+		ErrorCode code = mapper.getMapedErrorCode(ErrorCode.API_MANAGER_COMMUNICATION);
+		Assert.assertEquals(code, ErrorCode.API_MANAGER_COMMUNICATION);
+	}
+	
+	@Test
+	public void testMapper5() throws AppException, ParseException {
+		ErrorCodeMapper mapper = new ErrorCodeMapper();
+		mapper.setMapConfiguration("177:198");
+
+		ErrorCode code = mapper.getMapedErrorCode(ErrorCode.API_MANAGER_COMMUNICATION);
+		Assert.assertEquals(code, ErrorCode.API_MANAGER_COMMUNICATION);
+	}
 }
