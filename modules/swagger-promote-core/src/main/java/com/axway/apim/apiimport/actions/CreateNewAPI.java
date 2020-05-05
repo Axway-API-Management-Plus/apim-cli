26abeb2b0a1dc6650c438a19e4e8034dd6e71446
Chris Wiechmann <cwiechmann@axway.com>
2020-05-05 08:52:17 +0200

    Removed dead code for V-Host handling leaving a comment
    As the V-Host became a criteria, I assume this code becomes obsolete. With that, a V-Host cannot be updated anymore like any other property.

--- modules/swagger-promote-core/src/main/java/com/axway/apim/apiimport/actions/CreateNewAPI.java
+++ modules/swagger-promote-core/src/main/java/com/axway/apim/apiimport/actions/CreateNewAPI.java
@@ -110,9 +110,7 @@ public class CreateNewAPI {
 			// Handle subscription to applications
 			new ManageClientApps(changes.getDesiredAPI(), createdAPI, changes.getActualAPI()).execute(reCreation);
 			
-			// V-Host must be managed almost at the end, as the status must be set already to "published"
-			//vHostHandler.handleVHost(changes.getDesiredAPI(), createdAPI);
-			// Vhost should be setup during the api publish
+			// The V-Host is managed in UpdateAPIStatus
 		} catch (Exception e) {
 			throw e;
 		} finally {
