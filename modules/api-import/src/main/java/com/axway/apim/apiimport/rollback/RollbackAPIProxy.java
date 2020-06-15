c2afd462613478db7e84f58b95ffb4e73c4bf1a1
Chris Wiechmann <cwiechmann@axway.com>
2020-06-15 11:58:27 +0200

    Rollback-Handling improved

--- modules/api-import/src/main/java/com/axway/apim/apiimport/rollback/RollbackAPIProxy.java
+++ modules/api-import/src/main/java/com/axway/apim/apiimport/rollback/RollbackAPIProxy.java
@@ -20,19 +20,22 @@ public class RollbackAPIProxy extends AbstractRollbackAction implements Rollback
 
 	@Override
 	public void rollback() throws AppException {
-		if(rollbackAPI.getState()!=null && rollbackAPI.getState().equals(API.STATE_PUBLISHED)) {
+		if(rollbackAPI!=null && rollbackAPI.getState()==null) {
+			rollbackAPI = APIManagerAdapter.getInstance().apiAdapter.getAPI(new APIFilter.Builder().hasId(rollbackAPI.getId()).build(), false);
+		}
+		if(rollbackAPI.getState().equals(API.STATE_PUBLISHED)) {
 			new APIStatusManager().update(rollbackAPI, API.STATE_UNPUBLISHED, true);
 		}
 		try {
 			if(rollbackAPI.getId()!=null) { // We already have an ID to the FE-API can delete it directly
-				LOG.info("Rollback FE-API: '"+this.rollbackAPI.getName()+"' (ID: '"+this.rollbackAPI.getId()+"')");
+				LOG.info("Rollback FE-API: '"+this.rollbackAPI.getName()+"' (ID: '"+this.rollbackAPI.getId()+"' / State: '"+this.rollbackAPI.getState()+"')");
 				APIManagerAdapter.getInstance().apiAdapter.deleteAPIProxy(this.rollbackAPI);
 			} else {
 				// As we don't have the FE-API ID, try to find the FE-API, based on the BE-API-ID
 				APIFilter filter = new APIFilter.Builder().hasApiId(rollbackAPI.getApiId()).build(); 
 				API existingAPI = APIManagerAdapter.getInstance().apiAdapter.getAPI(filter, false);// The path is not set at this point, hence we provide null
 				if(existingAPI!=null) {
-					LOG.info("Rollback FE-API: '"+existingAPI.getName()+"' (ID: '"+existingAPI.getId()+"')");
+					LOG.info("Rollback FE-API: '"+existingAPI.getName()+"' (ID: '"+existingAPI.getId()+"' / State: '"+existingAPI.getState()+"')");
 					APIManagerAdapter.getInstance().apiAdapter.deleteAPIProxy(existingAPI);
 				} else {
 					LOG.info("No FE-API found to rollback.");
