7edde80821a2fb5404400c1514dcbc5d0fea0063
Chris Wiechmann <cwiechmann@axway.com>
2020-11-03 15:58:48 +0100

    Update pull_apim_docker_image.sh

--- build/pull_apim_docker_image.sh
+++ build/pull_apim_docker_image.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-set -x
-
-if [ -f $CACHE_FILE_APIM -a "$SKIP_CACHE"!="true" ]
+if [ -f $CACHE_FILE_APIM -a "$SKIP_CACHE" != "true" ]
 then
-	echo "Loading API-Manager docker image: $APIM_DOCKER_IMAGE from $CACHE_FILE_APIM"
+	echo "Using cached API-Manager docker image: $APIM_DOCKER_IMAGE from $CACHE_FILE_APIM"
 	gunzip -c $CACHE_FILE_APIM | docker load
 else
 	echo "Pulling APIM docker from registry, this will take a while"
