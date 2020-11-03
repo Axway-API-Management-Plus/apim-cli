e50912b96c425d24d0c146665f5e3514798884fe
Chris Wiechmann <cwiechmann@axway.com>
2020-11-03 15:27:26 +0100

    Changing the way how the docker-image is pulled

--- build/pull_apim_docker_image.sh
+++ build/pull_apim_docker_image.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+set -x
+
 if [ -f $CACHE_FILE_APIM -a ! $SKIP_CACHE="true" ]
 then
 	echo "Loading API-Manager docker image: $APIM_DOCKER_IMAGE from $CACHE_FILE_APIM"
 	gunzip -c $CACHE_FILE_APIM | docker load
 else
 	echo "Pulling APIM docker from registry, this will take a while"
+	docker login --username $AXWAY_DOCKER_REG_USER --password $AXWAY_DOCKER_REG_PASS docker-registry.demo.axway.com
 	docker pull $APIM_DOCKER_IMAGE
 fi
