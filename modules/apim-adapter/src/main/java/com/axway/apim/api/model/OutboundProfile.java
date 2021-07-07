2cf14b181f4091040fd05a9ffc0a87191c0f37f3
fabien Triolet <fabientriolet@Mac-mini-de-fabien.local>
2021-07-07 21:22:38 +0200

    add default values in getters for routeType and authenticationProfile

--- modules/apim-adapter/src/main/java/com/axway/apim/api/model/OutboundProfile.java
+++ modules/apim-adapter/src/main/java/com/axway/apim/api/model/OutboundProfile.java
@@ -15,146 +15,152 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 public class OutboundProfile extends Profile {
-	
-	protected static Logger LOG = LoggerFactory.getLogger(OutboundProfile.class);
-	
-	String routeType;
-	
-	@JsonDeserialize( using = PolicyDeserializer.class)
-	Policy requestPolicy;
-	
-	@JsonDeserialize( using = PolicyDeserializer.class)
-	Policy responsePolicy;
-	
-	@JsonDeserialize( using = PolicyDeserializer.class)
-	Policy routePolicy;
-	
-	@JsonDeserialize( using = PolicyDeserializer.class)
-	Policy faultHandlerPolicy;
-	
-	String authenticationProfile;
-	
-	List<Object> parameters = new ArrayList<Object>();
+    
+    protected static Logger LOG = LoggerFactory.getLogger(OutboundProfile.class);
+    
+    String routeType;
+    
+    @JsonDeserialize( using = PolicyDeserializer.class)
+    Policy requestPolicy;
+    
+    @JsonDeserialize( using = PolicyDeserializer.class)
+    Policy responsePolicy;
+    
+    @JsonDeserialize( using = PolicyDeserializer.class)
+    Policy routePolicy;
+    
+    @JsonDeserialize( using = PolicyDeserializer.class)
+    Policy faultHandlerPolicy;
+    
+    String authenticationProfile;
+    
+    List<Object> parameters = new ArrayList<Object>();
 
-	public OutboundProfile() throws AppException {
-		super();
-	}
+    public OutboundProfile() throws AppException {
+        super();
+    }
 
-	public String getAuthenticationProfile() {
-		return this.authenticationProfile;
-	}
+    public String getAuthenticationProfile() {
+        if(this.authenticationProfile==null){
+            return "_default"; 
+        }
+        if(this.authenticationProfile!=null && this.authenticationProfile.equals("")) {
+            return "_default";
+        }
+        return this.authenticationProfile;
+    }
 
-	public void setAuthenticationProfile(String authenticationProfile) {
-		this.authenticationProfile = authenticationProfile;
-	}
+    public void setAuthenticationProfile(String authenticationProfile) {
+        this.authenticationProfile = authenticationProfile;
+    }
 
-	public String getRouteType() {
-		if(this.routePolicy!=null && !this.routePolicy.equals("")) {
-			return "policy";
-		} else {
-			return "proxy";
-		}
-	}
+    public String getRouteType() {
+        if(this.routeType!=null && !this.routeType.equals("") && !this.routeType.equals("proxy") ) {
+            return "policy";
+        } else {
+            return "proxy";
+        }
+    }
 
-	public void setRouteType(String routeType) {
-		this.routeType = routeType;
-	}
+    public void setRouteType(String routeType) {
+        this.routeType = routeType;
+    }
 
-	public Policy getRequestPolicy() {
-		return requestPolicy;
-	}
-	
-	public void setRequestPolicy(Policy requestPolicy) throws AppException {
-		this.requestPolicy = requestPolicy;
-	}
+    public Policy getRequestPolicy() {
+        return requestPolicy;
+    }
+    
+    public void setRequestPolicy(Policy requestPolicy) throws AppException {
+        this.requestPolicy = requestPolicy;
+    }
 
-	public Policy getResponsePolicy() {
-		return responsePolicy;
-	}
+    public Policy getResponsePolicy() {
+        return responsePolicy;
+    }
 
-	public void setResponsePolicy(Policy responsePolicy) throws AppException {
-		this.responsePolicy = responsePolicy;
-	}
+    public void setResponsePolicy(Policy responsePolicy) throws AppException {
+        this.responsePolicy = responsePolicy;
+    }
 
-	public Policy getRoutePolicy() {
-		return routePolicy;
-	}
+    public Policy getRoutePolicy() {
+        return routePolicy;
+    }
 
-	public void setRoutePolicy(Policy routePolicy) throws AppException {
-		this.routePolicy = routePolicy;
-	}
+    public void setRoutePolicy(Policy routePolicy) throws AppException {
+        this.routePolicy = routePolicy;
+    }
 
-	public Policy getFaultHandlerPolicy() {
-		return faultHandlerPolicy;
-	}
+    public Policy getFaultHandlerPolicy() {
+        return faultHandlerPolicy;
+    }
 
-	public void setFaultHandlerPolicy(Policy faultHandlerPolicy) throws AppException {
-		this.faultHandlerPolicy = faultHandlerPolicy;
-	}
+    public void setFaultHandlerPolicy(Policy faultHandlerPolicy) throws AppException {
+        this.faultHandlerPolicy = faultHandlerPolicy;
+    }
 
-	public List<Object> getParameters() {
-		if(parameters==null || parameters.size()==0) return null;
-		return parameters;
-	}
-	
-	@JsonIgnore
-	public List<Policy> getAllPolices() {
-		List<Policy> usedPolicies = new ArrayList<Policy>();
-		if(this.requestPolicy!=null) usedPolicies.add(this.requestPolicy);
-		if(this.routePolicy!=null) usedPolicies.add(this.routePolicy);
-		if(this.responsePolicy!=null) usedPolicies.add(this.responsePolicy);
-		if(this.faultHandlerPolicy!=null) usedPolicies.add(this.faultHandlerPolicy);
-		return usedPolicies;
-	}
+    public List<Object> getParameters() {
+        if(parameters==null || parameters.size()==0) return null;
+        return parameters;
+    }
+    
+    @JsonIgnore
+    public List<Policy> getAllPolices() {
+        List<Policy> usedPolicies = new ArrayList<Policy>();
+        if(this.requestPolicy!=null) usedPolicies.add(this.requestPolicy);
+        if(this.routePolicy!=null) usedPolicies.add(this.routePolicy);
+        if(this.responsePolicy!=null) usedPolicies.add(this.responsePolicy);
+        if(this.faultHandlerPolicy!=null) usedPolicies.add(this.faultHandlerPolicy);
+        return usedPolicies;
+    }
 
 
-	public void setParameters(List<Object> parameters) {
-		if(APIManagerAdapter.hasAPIManagerVersion("7.7.20200130")) {
-			// We need to inject the format as default
-			for(Object params : parameters) {
-				if(params instanceof Map<?, ?>) {
-					if(!((Map<?, ?>)params).containsKey("format")) {
-						((Map<String, ?>) params).put("format", null);
-					}
-				}
-			}
-		}
-		this.parameters = parameters;
-	}
+    public void setParameters(List<Object> parameters) {
+        if(APIManagerAdapter.hasAPIManagerVersion("7.7.20200130")) {
+            // We need to inject the format as default
+            for(Object params : parameters) {
+                if(params instanceof Map<?, ?>) {
+                    if(!((Map<?, ?>)params).containsKey("format")) {
+                        ((Map<String, ?>) params).put("format", null);
+                    }
+                }
+            }
+        }
+        this.parameters = parameters;
+    }
 
-	@Override
-	public boolean equals(Object other) {
-		if(other == null) return false;
-		if(other instanceof OutboundProfile) {
-			OutboundProfile otherOutboundProfile = (OutboundProfile)other;
-			List<Object> otherParameters = otherOutboundProfile.getParameters();
-			List<Object> thisParameters = this.getParameters();
-			if(APIManagerAdapter.hasAPIManagerVersion("7.7 SP1") || APIManagerAdapter.hasAPIManagerVersion("7.6.2 SP5")) {
-				// Passwords no longer exposed by API-Manager REST-API - Can't use it anymore to compare the state
-				if(otherParameters!=null) otherParameters.remove("password");
-				if(thisParameters!=null) thisParameters.remove("password");
-			}
-			boolean rc = 
-				policiesAreEqual(this.getFaultHandlerPolicy(), otherOutboundProfile.getFaultHandlerPolicy()) &&
-				policiesAreEqual(this.getRequestPolicy(), otherOutboundProfile.getRequestPolicy()) &&
-				policiesAreEqual(this.getResponsePolicy(), otherOutboundProfile.getResponsePolicy()) &&
-				policiesAreEqual(this.getRoutePolicy(), otherOutboundProfile.getRoutePolicy()) &&
-				StringUtils.equals(otherOutboundProfile.getRouteType(), this.getRouteType()) &&
-				StringUtils.equals(otherOutboundProfile.getAuthenticationProfile(), this.getAuthenticationProfile()) &&
-				(thisParameters==null || thisParameters.equals(otherParameters));
-			return rc;
-		} else {
-			return false;
-		}
-	}
+    @Override
+    public boolean equals(Object other) {
+        if(other == null) return false;
+        if(other instanceof OutboundProfile) {
+            OutboundProfile otherOutboundProfile = (OutboundProfile)other;
+            List<Object> otherParameters = otherOutboundProfile.getParameters();
+            List<Object> thisParameters = this.getParameters();
+            if(APIManagerAdapter.hasAPIManagerVersion("7.7 SP1") || APIManagerAdapter.hasAPIManagerVersion("7.6.2 SP5")) {
+                // Passwords no longer exposed by API-Manager REST-API - Can't use it anymore to compare the state
+                if(otherParameters!=null) otherParameters.remove("password");
+                if(thisParameters!=null) thisParameters.remove("password");
+            }
+            boolean rc = 
+                policiesAreEqual(this.getFaultHandlerPolicy(), otherOutboundProfile.getFaultHandlerPolicy()) &&
+                policiesAreEqual(this.getRequestPolicy(), otherOutboundProfile.getRequestPolicy()) &&
+                policiesAreEqual(this.getResponsePolicy(), otherOutboundProfile.getResponsePolicy()) &&
+                policiesAreEqual(this.getRoutePolicy(), otherOutboundProfile.getRoutePolicy()) &&
+                StringUtils.equalsIgnoreCase(this.getRouteType(), otherOutboundProfile.getRouteType()) &&
+                StringUtils.equalsIgnoreCase(this.getAuthenticationProfile(), otherOutboundProfile.getAuthenticationProfile()) &&
+                (thisParameters==null || thisParameters.equals(otherParameters));
+            return rc;
+        } else {
+            return false;
+        }
+    }
 
-	@Override
-	public String toString() {
-		return "OutboundProfile [routeType="+routeType+", requestPolicy=" + requestPolicy + ", responsePolicy=" + responsePolicy+ ", routePolicy=" + routePolicy+ ", faultHandlerPolicy="+ faultHandlerPolicy +", authenticationProfile="+authenticationProfile+"]";
-	}
-	
-	private boolean policiesAreEqual(Policy policyA, Policy policyB) {
-		if(policyA==null && policyB == null) return true;
-		return (policyA!=null && policyA.equals(policyB));
-	}
+    @Override
+    public String toString() {
+        return "OutboundProfile [routeType="+routeType+", requestPolicy=" + requestPolicy + ", responsePolicy=" + responsePolicy+ ", routePolicy=" + routePolicy+ ", faultHandlerPolicy="+ faultHandlerPolicy +", authenticationProfile="+authenticationProfile+"]";
+    }
+
+    private boolean policiesAreEqual(Policy policyA, Policy policyB) {
+        if(policyA==null && policyB == null) return true;
+        return (policyA!=null && policyA.equals(policyB));
+    }
 }
--- modules/apim-adapter/src/test/java/com/axway/apim/api/model/OutboundProfileTest.java
+++ modules/apim-adapter/src/test/java/com/axway/apim/api/model/OutboundProfileTest.java
@@ -6,91 +6,170 @@ import org.testng.annotations.Test;
 import com.axway.apim.lib.errorHandling.AppException;
 
 public class OutboundProfileTest {
-	@Test
-	public void testWithCustomRequestPolicy() throws AppException {
-		OutboundProfile actualProfile = new OutboundProfile();
-		OutboundProfile desiredProfile = new OutboundProfile();
-		
-		actualProfile.setRequestPolicy(null);
-		desiredProfile.setRequestPolicy(new Policy("My custom request policy"));
-		
-		Assert.assertFalse(actualProfile.equals(desiredProfile), "Outbound profiles must be different");
-	}
-	
-	@Test
-	public void testWithCustomResponsePolicy() throws AppException {
-		OutboundProfile actualProfile = new OutboundProfile();
-		OutboundProfile desiredProfile = new OutboundProfile();
-		
-		actualProfile.setResponsePolicy(null);
-		desiredProfile.setResponsePolicy(new Policy("My custom response policy"));
-		
-		Assert.assertFalse(actualProfile.equals(desiredProfile), "Outbound profiles must be different");
-	}
-	
-	@Test
-	public void testWithCustomSameResponsePolicy() throws AppException {
-		OutboundProfile actualProfile = new OutboundProfile();
-		OutboundProfile desiredProfile = new OutboundProfile();
-		
-		actualProfile.setResponsePolicy(new Policy("My custom response policy"));
-		desiredProfile.setResponsePolicy(new Policy("My custom response policy"));
-		
-		Assert.assertTrue(actualProfile.equals(desiredProfile), "Outbound profiles must be equal");
-	}
-	
-	@Test
-	public void testWithCustomRoutePolicy() throws AppException {
-		OutboundProfile actualProfile = new OutboundProfile();
-		OutboundProfile desiredProfile = new OutboundProfile();
-		
-		actualProfile.setRoutePolicy(null);
-		desiredProfile.setRoutePolicy(new Policy("My custom routing policy"));
-		
-		Assert.assertFalse(actualProfile.equals(desiredProfile), "Outbound profiles must be different");
-	}
-	
-	@Test
-	public void testWithCustomSameRoutingPolicy() throws AppException {
-		OutboundProfile actualProfile = new OutboundProfile();
-		OutboundProfile desiredProfile = new OutboundProfile();
-		
-		actualProfile.setRoutePolicy(new Policy("My custom routing policy"));
-		desiredProfile.setRoutePolicy(new Policy("My custom routing policy"));
-		
-		Assert.assertTrue(actualProfile.equals(desiredProfile), "Outbound profiles must be equal");
-	}
-	
-	@Test
-	public void testWithCustomFaultHandlerPolicy() throws AppException {
-		OutboundProfile actualProfile = new OutboundProfile();
-		OutboundProfile desiredProfile = new OutboundProfile();
-		
-		actualProfile.setFaultHandlerPolicy(null);
-		desiredProfile.setFaultHandlerPolicy(new Policy("My custom fault handler policy"));
-		
-		Assert.assertFalse(actualProfile.equals(desiredProfile), "Outbound profiles must be different");
-	}
-	
-	@Test
-	public void testWithCustomSameFaultHandlerPolicy() throws AppException {
-		OutboundProfile actualProfile = new OutboundProfile();
-		OutboundProfile desiredProfile = new OutboundProfile();
-		
-		actualProfile.setFaultHandlerPolicy(new Policy("My custom fault handler policy"));
-		desiredProfile.setFaultHandlerPolicy(new Policy("My custom fault handler policy"));
-		
-		Assert.assertTrue(actualProfile.equals(desiredProfile), "Outbound profiles must be equal");
-	}
-	
-	@Test
-	public void testWithDifferentAuthNProfile() throws AppException {
-		OutboundProfile actualProfile = new OutboundProfile();
-		OutboundProfile desiredProfile = new OutboundProfile();
-		
-		actualProfile.setAuthenticationProfile("Passthrough");
-		desiredProfile.setAuthenticationProfile("Another profile");
-		
-		Assert.assertFalse(actualProfile.equals(desiredProfile), "The AuthN-Profile of the outbound profiles are different.");
-	}
+    @Test
+    public void testWithCustomRequestPolicy() throws AppException {
+        OutboundProfile actualProfile = new OutboundProfile();
+        OutboundProfile desiredProfile = new OutboundProfile();
+        
+        actualProfile.setRequestPolicy(null);
+        desiredProfile.setRequestPolicy(new Policy("My custom request policy"));
+        
+        Assert.assertFalse(actualProfile.equals(desiredProfile), "Outbound profiles must be different");
+    }
+    
+    @Test
+    public void testWithCustomResponsePolicy() throws AppException {
+        OutboundProfile actualProfile = new OutboundProfile();
+        OutboundProfile desiredProfile = new OutboundProfile();
+        
+        actualProfile.setResponsePolicy(null);
+        desiredProfile.setResponsePolicy(new Policy("My custom response policy"));
+        
+        Assert.assertFalse(actualProfile.equals(desiredProfile), "Outbound profiles must be different");
+    }
+    
+    @Test
+    public void testWithCustomSameResponsePolicy() throws AppException {
+        OutboundProfile actualProfile = new OutboundProfile();
+        OutboundProfile desiredProfile = new OutboundProfile();
+        
+        actualProfile.setResponsePolicy(new Policy("My custom response policy"));
+        desiredProfile.setResponsePolicy(new Policy("My custom response policy"));
+        
+        Assert.assertTrue(actualProfile.equals(desiredProfile), "Outbound profiles must be equal");
+    }
+    
+    @Test
+    public void testWithCustomRoutePolicy() throws AppException {
+        OutboundProfile actualProfile = new OutboundProfile();
+        OutboundProfile desiredProfile = new OutboundProfile();
+        
+        actualProfile.setRoutePolicy(null);
+        desiredProfile.setRoutePolicy(new Policy("My custom routing policy"));
+        
+        Assert.assertFalse(actualProfile.equals(desiredProfile), "Outbound profiles must be different");
+    }
+    
+    @Test
+    public void testWithCustomSameRoutingPolicy() throws AppException {
+        OutboundProfile actualProfile = new OutboundProfile();
+        OutboundProfile desiredProfile = new OutboundProfile();
+        
+        actualProfile.setRoutePolicy(new Policy("My custom routing policy"));
+        desiredProfile.setRoutePolicy(new Policy("My custom routing policy"));
+        
+        Assert.assertTrue(actualProfile.equals(desiredProfile), "Outbound profiles must be equal");
+    }
+    
+    @Test
+    public void testWithCustomFaultHandlerPolicy() throws AppException {
+        OutboundProfile actualProfile = new OutboundProfile();
+        OutboundProfile desiredProfile = new OutboundProfile();
+        
+        actualProfile.setFaultHandlerPolicy(null);
+        desiredProfile.setFaultHandlerPolicy(new Policy("My custom fault handler policy"));
+        
+        Assert.assertFalse(actualProfile.equals(desiredProfile), "Outbound profiles must be different");
+    }
+    
+    @Test
+    public void testWithCustomSameFaultHandlerPolicy() throws AppException {
+        OutboundProfile actualProfile = new OutboundProfile();
+        OutboundProfile desiredProfile = new OutboundProfile();
+        
+        actualProfile.setFaultHandlerPolicy(new Policy("My custom fault handler policy"));
+        desiredProfile.setFaultHandlerPolicy(new Policy("My custom fault handler policy"));
+        
+        Assert.assertTrue(actualProfile.equals(desiredProfile), "Outbound profiles must be equal");
+    }
+    
+    @Test
+    public void testWithDifferentAuthNProfile() throws AppException {
+        OutboundProfile actualProfile = new OutboundProfile();
+        OutboundProfile desiredProfile = new OutboundProfile();
+        
+        actualProfile.setAuthenticationProfile("Passthrough");
+        desiredProfile.setAuthenticationProfile("Another profile");
+        
+        Assert.assertFalse(actualProfile.equals(desiredProfile), "The AuthN-Profile of the outbound profiles are different.");
+    }
+
+    @Test
+    public void testDefaultValueAuthNProfile() throws AppException {
+        OutboundProfile actualProfile = new OutboundProfile();
+        OutboundProfile desiredProfile = new OutboundProfile();
+        
+        actualProfile.setAuthenticationProfile(null);
+        desiredProfile.setAuthenticationProfile("_default");
+        
+        Assert.assertTrue(actualProfile.equals(desiredProfile), "The AuthN-Profile of the outbound profiles are different.");
+
+        actualProfile.setAuthenticationProfile("_default");
+        desiredProfile.setAuthenticationProfile("_default");
+
+        Assert.assertTrue(actualProfile.equals(desiredProfile), "The AuthN-Profile of the outbound profiles are different.");
+
+        actualProfile.setAuthenticationProfile("_default");
+        desiredProfile.setAuthenticationProfile(null);
+
+        Assert.assertTrue(actualProfile.equals(desiredProfile), "The AuthN-Profile of the outbound profiles are different.");
+
+        actualProfile.setAuthenticationProfile(null);
+        desiredProfile.setAuthenticationProfile(null);
+
+        Assert.assertTrue(actualProfile.equals(desiredProfile), "The AuthN-Profile of the outbound profiles are different.");
+    }
+
+    @Test
+    public void testWithDifferentARouteType() throws AppException {
+        OutboundProfile actualProfile = new OutboundProfile();
+        OutboundProfile desiredProfile = new OutboundProfile();
+        
+        actualProfile.setRouteType("proxy");
+        desiredProfile.setRouteType("policy");
+        
+        Assert.assertFalse(actualProfile.equals(desiredProfile), "The route type of the outbound profiles are equals.");
+    }
+
+    @Test
+    public void testDefaultValueRoute() throws AppException {
+        OutboundProfile actualProfile = new OutboundProfile();
+        OutboundProfile desiredProfile = new OutboundProfile();
+        
+        actualProfile.setRouteType(null);
+        desiredProfile.setRouteType("proxy");
+        
+        Assert.assertTrue(actualProfile.equals(desiredProfile), "The route type of the outbound profiles are different.");
+
+        actualProfile.setRouteType("proxy");
+        desiredProfile.setRouteType("proxy");
+
+        Assert.assertTrue(actualProfile.equals(desiredProfile), "The route type of the outbound profiles are different.");
+
+        actualProfile.setRouteType("proxy");
+        desiredProfile.setRouteType(null);
+
+        Assert.assertTrue(actualProfile.equals(desiredProfile), "The Aroute type of the outbound profiles are different.");
+
+        actualProfile.setRouteType(null);
+        desiredProfile.setRouteType(null);
+
+        Assert.assertTrue(actualProfile.equals(desiredProfile), "The route type of the outbound profiles are different.");
+
+        actualProfile.setRouteType("");
+        desiredProfile.setRouteType("");
+
+        Assert.assertTrue(actualProfile.equals(desiredProfile), "The route type of the outbound profiles are different.");
+
+        actualProfile.setRouteType("policy");
+        desiredProfile.setRouteType("policy");
+
+        Assert.assertTrue(actualProfile.equals(desiredProfile), "The route type of the outbound profiles are different.");
+
+        actualProfile.setRouteType("policy");
+        desiredProfile.setRouteType("proxy");
+
+        Assert.assertFalse(actualProfile.equals(desiredProfile), "The route type of the outbound profiles are equals.");
+    }
+
 }
