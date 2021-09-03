71474b08a8d39c44a5fa6dadd0ebe060e6e6b4e3
Chris Wiechmann <cwiechmann@axway.com>
2021-09-03 14:33:31 +0200

    [skip ci] Fixed the name of the FED-File for the Aug-Release.

--- modules/apim-adapter/src/test/resources/apimanager/buildDockerImage.sh
+++ modules/apim-adapter/src/test/resources/apimanager/buildDockerImage.sh
@@ -38,7 +38,7 @@ echo "Creating docker image for version $version"
 
 case "$version" in
 	7.7-20210830)
-		fedFile="swagger-promote-7.7-20210530.fed"
+		fedFile="swagger-promote-7.7-20210830.fed"
 		installer="apigw-installer-7.7.0.20210830-1-linux64.run";;
 	7.7-20210530)
 		fedFile="swagger-promote-7.7-20210530.fed"
