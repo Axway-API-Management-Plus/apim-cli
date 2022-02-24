665a033b36c0db6f21781a757b694857bce36307
Chris Wiechmann <cwiechmann@axway.com>
2022-02-24 12:24:45 +0100

    [skip ci] Added FED File for 2022 / Feb-Release

--- modules/apim-adapter/src/test/resources/apimanager/buildDockerImage.sh
+++ modules/apim-adapter/src/test/resources/apimanager/buildDockerImage.sh
@@ -9,6 +9,7 @@ function exitScript() {
 	cd $currentDir
 	if [[ $rc = 10 ]]; then
 		echo "Supported versions"
+		echo "`basename $0` 7.7-20220228"
 		echo "`basename $0` 7.7-20211130"
 		echo "`basename $0` 7.7-20210830"
 		echo "`basename $0` 7.7-20210530"
@@ -38,6 +39,10 @@ buildDir="$HOME/apim-cli-dockerimage"
 echo "Creating docker image for version $version"
 
 case "$version" in
+	7.7-20220228)
+		fedFile="swagger-promote-7.7-20211130.fed"
+		installer="apigw-installer-7.7.0.20220228-1-linux64.run";;
+		dockerScripts="apigw-emt-scripts-2.4.0-20220222.150412-10.tar.gz";;
 	7.7-20211130)
 		fedFile="swagger-promote-7.7-20211130.fed"
 		installer="apigw-installer-7.7.0.20211130-1-linux64.run";;
