cf3279c2c99ce086fcd0bc3ec05e8554e78c17d1
Rathnapandi <rathnapandi.n@gmail.com>
2022-08-15 19:12:43 -0700

    - Code cleanup

--- modules/apim-adapter/src/main/java/com/axway/apim/api/model/TagMap.java
+++ modules/apim-adapter/src/main/java/com/axway/apim/api/model/TagMap.java
@@ -1,45 +1,30 @@
 package com.axway.apim.api.model;
 
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Objects;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public class TagMap<K, V> extends LinkedHashMap<String, String[]> {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-	public TagMap() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-
-
-	@Override
-	public boolean equals(Object o) {
-		if (o == this) return true;
-		
-		if (!(o instanceof TagMap)) return false;
-		
-		TagMap<String,String[]> otherTagMap = (TagMap<String,String[]>) o;
-		
-		if (otherTagMap.size() != size()) return false;
-		
-		Iterator<String> it = this.keySet().iterator();
-		
-		while(it.hasNext()) {
-			String tagName = it.next();
-			if(!otherTagMap.containsKey(tagName)) return false;
-			String[] myTags = this.get(tagName);
-			String[] otherTags = otherTagMap.get(tagName);
-			if(!Objects.deepEquals(myTags, otherTags)) return false;
-		}
-		return true;
-	}
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    public TagMap() {
+        super();
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof TagMap)) return false;
+        TagMap<String, String[]> otherTagMap = (TagMap<String, String[]>) o;
+        if (otherTagMap.size() != size()) return false;
+        for (String tagName : this.keySet()) {
+            if (!otherTagMap.containsKey(tagName)) return false;
+            String[] myTags = this.get(tagName);
+            String[] otherTags = otherTagMap.get(tagName);
+            if (!Objects.deepEquals(myTags, otherTags)) return false;
+        }
+        return true;
+    }
 
 }
