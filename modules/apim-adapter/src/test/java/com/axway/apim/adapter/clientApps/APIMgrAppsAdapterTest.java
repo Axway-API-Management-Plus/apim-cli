9e43afbfdeb4952d34b0d5c0f7bb9607b23f2e2c
Rathnapandi <rathnapandi.n@gmail.com>
2023-01-20 11:38:02 -0700

    - Add junit

--- modules/apim-adapter/src/test/java/com/axway/apim/adapter/clientApps/APIMgrAppsAdapterTest.java
+++ modules/apim-adapter/src/test/java/com/axway/apim/adapter/clientApps/APIMgrAppsAdapterTest.java
@@ -1,36 +1,49 @@
 package com.axway.apim.adapter.clientApps;
 
+import com.axway.apim.WiremockWrapper;
+import com.axway.apim.adapter.APIManagerAdapter;
+import com.axway.apim.api.model.apps.ClientApplication;
+import com.axway.apim.lib.CoreParameters;
+import com.axway.apim.lib.errorHandling.AppException;
+import com.axway.apim.lib.utils.TestIndicator;
+import com.axway.apim.lib.utils.Utils;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+public class APIMgrAppsAdapterTest extends WiremockWrapper {
 
-import com.axway.apim.lib.CoreParameters;
-import com.axway.apim.lib.errorHandling.AppException;
-import com.axway.apim.lib.utils.TestIndicator;
+	private final String testHostname = "localhost";
+	private final int testPort = 8075;
 
-public class APIMgrAppsAdapterTest {
-	
-	private String testHostname = "api-manager-host";
-	private int testPort = 8088;
-	
 	@BeforeClass
-	private void initTestIndicator() {
+	public void initWiremock() {
 		TestIndicator.getInstance().setTestRunning(true);
-		CoreParameters params = new CoreParameters();
-		params.setHostname(testHostname);
-		params.setPort(testPort);
+		super.initWiremock();
+		CoreParameters coreParameters = new CoreParameters();
+		coreParameters.setHostname(testHostname);
+		coreParameters.setPort(testPort);
+		coreParameters.setUsername("test");
+		coreParameters.setPassword(Utils.getEncryptedPassword());
 	}
-	
+
+	@AfterClass
+	public void close() {
+		super.close();
+	}
+
+
 	@Test
-	public void queryForUniqueApplication() throws AppException, IOException, URISyntaxException {
+	public void queryForUniqueApplication() throws IOException, URISyntaxException {
 		APIMgrAppsAdapter clientAppAdapter = new APIMgrAppsAdapter();
 		ClientAppFilter filter = new ClientAppFilter.Builder().hasName("Application 123").build();
 		URI requestUri = clientAppAdapter.getApplicationsUri(filter);
@@ -39,7 +52,7 @@ public class APIMgrAppsAdapterTest {
 	}
 	
 	@Test
-	public void withoutAnyFilter() throws AppException, IOException, URISyntaxException {
+	public void withoutAnyFilter() throws IOException, URISyntaxException {
 		APIMgrAppsAdapter clientAppAdapter = new APIMgrAppsAdapter();
 		URI requestUri = clientAppAdapter.getApplicationsUri(null);
 		Assert.assertNotNull(requestUri, "RequestUri is null");
@@ -47,7 +60,7 @@ public class APIMgrAppsAdapterTest {
 	}
 	
 	@Test
-	public void usingApplicationId() throws AppException, IOException, URISyntaxException {
+	public void usingApplicationId() throws IOException, URISyntaxException {
 		APIMgrAppsAdapter clientAppAdapter = new APIMgrAppsAdapter();
 		ClientAppFilter filter = new ClientAppFilter.Builder().hasId("5893475934875934").build();
 		URI requestUri = clientAppAdapter.getApplicationsUri(filter);
@@ -57,7 +70,7 @@ public class APIMgrAppsAdapterTest {
 	}
 	
 	@Test
-	public void filterForAppName() throws AppException, IOException, URISyntaxException {
+	public void filterForAppName() throws IOException, URISyntaxException {
 		APIMgrAppsAdapter clientAppAdapter = new APIMgrAppsAdapter();
 		ClientAppFilter filter = new ClientAppFilter.Builder().hasName("MyTestApp").build();
 		URI requestUri = clientAppAdapter.getApplicationsUri(filter);
@@ -67,7 +80,7 @@ public class APIMgrAppsAdapterTest {
 	}
 	
 	@Test
-	public void filterForOrgId() throws AppException, IOException, URISyntaxException {
+	public void filterForOrgId() throws IOException, URISyntaxException {
 		APIMgrAppsAdapter clientAppAdapter = new APIMgrAppsAdapter();
 		ClientAppFilter filter = new ClientAppFilter.Builder().hasOrganizationId("42342342342343223").build();
 		URI requestUri = clientAppAdapter.getApplicationsUri(filter);
@@ -77,7 +90,7 @@ public class APIMgrAppsAdapterTest {
 	}
 	
 	@Test
-	public void filterStatePending() throws AppException, IOException, URISyntaxException {
+	public void filterStatePending() throws IOException, URISyntaxException {
 		APIMgrAppsAdapter clientAppAdapter = new APIMgrAppsAdapter();
 		ClientAppFilter filter = new ClientAppFilter.Builder().hasState("pending").build();
 		URI requestUri = clientAppAdapter.getApplicationsUri(filter);
@@ -87,7 +100,7 @@ public class APIMgrAppsAdapterTest {
 	}
 	
 	@Test
-	public void filterStatePendingAndAppName() throws AppException, IOException, URISyntaxException {
+	public void filterStatePendingAndAppName() throws IOException, URISyntaxException {
 		APIMgrAppsAdapter clientAppAdapter = new APIMgrAppsAdapter();
 		
 		ClientAppFilter filter = new ClientAppFilter.Builder().hasState("pending").hasName("AnotherPendingApp").build();
@@ -98,8 +111,8 @@ public class APIMgrAppsAdapterTest {
 	}
 	
 	@Test
-	public void filterCustomFieldAndName() throws AppException, IOException, URISyntaxException {
-		List<NameValuePair> customFilters = new ArrayList<NameValuePair>();
+	public void filterCustomFieldAndName() throws IOException, URISyntaxException {
+		List<NameValuePair> customFilters = new ArrayList<>();
 		customFilters.add(new BasicNameValuePair("field", "email"));
 		customFilters.add(new BasicNameValuePair("op", "eq"));
 		customFilters.add(new BasicNameValuePair("value", "this@there.com"));
@@ -117,7 +130,7 @@ public class APIMgrAppsAdapterTest {
 	}
 	
 	@Test
-	public void filterNullValues() throws AppException, IOException, URISyntaxException {		
+	public void filterNullValues() throws IOException, URISyntaxException {
 		APIMgrAppsAdapter clientAppAdapter = new APIMgrAppsAdapter();
 		
 		ClientAppFilter filter = new ClientAppFilter.Builder().hasState(null).hasName(null).hasOrganizationId(null).build();
@@ -126,5 +139,132 @@ public class APIMgrAppsAdapterTest {
 		Assert.assertNotNull(requestUri, "RequestUri is null");
 		Assert.assertEquals(requestUri.toString(), "https://"+testHostname+":"+testPort+"/api/portal/v1.4/applications");
 	}
+
+
+	@Test
+	public void getApplications() throws AppException {
+		APIManagerAdapter.deleteInstance();
+		APIMgrAppsAdapter appAdapter = APIManagerAdapter.getInstance().appAdapter;
+		List<ClientApplication> clientApplications = appAdapter.getAllApplications(false);
+		Assert.assertNotNull(clientApplications);
+	}
+
+	@Test
+	public void getAppsSubscribedWithAPI() throws AppException {
+		APIManagerAdapter.deleteInstance();
+		APIMgrAppsAdapter appAdapter = APIManagerAdapter.getInstance().appAdapter;
+		List<ClientApplication> clientApplications = appAdapter.getAppsSubscribedWithAPI("e4ded8c8-0a40-4b50-bc13-552fb7209150");
+		Assert.assertNotNull(clientApplications);
+	}
+
+	@Test
+	public void getApplication() throws AppException {
+		APIManagerAdapter.deleteInstance();
+		APIMgrAppsAdapter appAdapter = APIManagerAdapter.getInstance().appAdapter;
+		ClientApplication clientApplication = appAdapter.getApplication(new ClientAppFilter.Builder().hasName("Test App 2008").build());
+		Assert.assertEquals(clientApplication.getName(), "Test App 2008");
+	}
+
+
+	@Test
+	public void deleteApplication() throws AppException {
+		APIManagerAdapter.deleteInstance();
+		APIMgrAppsAdapter appAdapter = APIManagerAdapter.getInstance().appAdapter;
+		ClientApplication clientApplication = appAdapter.getApplication(new ClientAppFilter.Builder().hasName("Test App 2008").build());
+		try {
+			appAdapter.deleteApplication(clientApplication);
+
+		} catch (AppException appException) {
+			Assert.fail("unable to delete application", appException);
+		}
+	}
+
+	@Test
+	public void updateApplication() throws AppException {
+		APIManagerAdapter.deleteInstance();
+		APIMgrAppsAdapter appAdapter = APIManagerAdapter.getInstance().appAdapter;
+		ClientApplication clientApplication = appAdapter.getApplication(new ClientAppFilter.Builder().hasName("Test App 2008").build());
+
+		ClientApplication updatedApplication = new ClientApplication();
+		updatedApplication.setName("test");
+		updatedApplication.setId(clientApplication.getId());
+		try {
+			appAdapter.createOrUpdateApplication(updatedApplication, clientApplication);
+
+		} catch (AppException appException) {
+			Assert.fail("unable to update application", appException);
+		}
+	}
+
+	@Test
+	public void createApplication() throws AppException {
+		APIManagerAdapter.deleteInstance();
+		APIMgrAppsAdapter appAdapter = APIManagerAdapter.getInstance().appAdapter;
+		ClientApplication clientApplication = new ClientApplication();
+		clientApplication.setName("test");
+		try {
+			appAdapter.createApplication(clientApplication);
+
+		} catch (AppException appException) {
+			Assert.fail("unable to create application", appException);
+		}
+	}
+//
+//	@Test
+//	public void updateUser() throws AppException {
+//		setupParameters();
+//		APIManagerAdapter apiManagerAdapter = APIManagerAdapter.getInstance();
+//		APIManagerUserAdapter apiManagerUserAdapter = apiManagerAdapter.userAdapter;
+//		UserFilter userFilter = new UserFilter.Builder().hasLoginName(loginName).build();
+//		User user = apiManagerUserAdapter.getUser(userFilter);
+//		User desiredUser = new User();
+//		desiredUser.setEmail("updated@axway.com");
+//		desiredUser.setName(user.getName());
+//		desiredUser.setLoginName(user.getLoginName());
+//		User newUser = apiManagerUserAdapter.updateUser(desiredUser, user);
+//		Assert.assertEquals(newUser.getEmail(), "updated@axway.com");
+//	}
+//
+//	@Test
+//	public void updateUserCreateNewUserFlow() throws AppException {
+//		setupParameters();
+//		APIManagerAdapter apiManagerAdapter = APIManagerAdapter.getInstance();
+//		APIManagerUserAdapter apiManagerUserAdapter = apiManagerAdapter.userAdapter;
+//		User user = new User();
+//		user.setEmail("updated@axway.com");
+//		user.setName("usera");
+//		user.setLoginName("usera");
+//		User newUser = apiManagerUserAdapter.updateUser(user, null);
+//		Assert.assertEquals(newUser.getEmail(), "updated@axway.com");
+//	}
+//
+//	@Test
+//	public void changePassword() throws AppException {
+//		setupParameters();
+//		APIManagerAdapter apiManagerAdapter = APIManagerAdapter.getInstance();
+//		APIManagerUserAdapter apiManagerUserAdapter = apiManagerAdapter.userAdapter;
+//		UserFilter userFilter = new UserFilter.Builder().hasLoginName(loginName).build();
+//		User user = apiManagerUserAdapter.getUser(userFilter);
+//		try {
+//			apiManagerUserAdapter.changePassword(Utils.getEncryptedPassword(), user);
+//		} catch (AppException appException) {
+//			Assert.fail("unable to change user password", appException);
+//		}
+//	}
+//
+//	@Test
+//	public void addImage() throws AppException {
+//		setupParameters();
+//		APIManagerAdapter apiManagerAdapter = APIManagerAdapter.getInstance();
+//		APIManagerUserAdapter apiManagerUserAdapter = apiManagerAdapter.userAdapter;
+//		UserFilter userFilter = new UserFilter.Builder().hasLoginName(loginName).build();
+//		User user = apiManagerUserAdapter.getUser(userFilter);
+//		user.setImageUrl("https://axway.com/favicon.ico");
+//		try {
+//			apiManagerUserAdapter.addImage(user, true);
+//		} catch (AppException appException) {
+//			Assert.fail("unable to add Image", appException);
+//		}
+//	}
 	
 }
