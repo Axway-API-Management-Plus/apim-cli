826a7edd5e0a7d206a8bede682eef5b742c61ab5
Rathnapandi <rathnapandi.n@gmail.com>
2023-01-19 17:43:58 -0700

    - Add junit

---
+++ modules/users/src/test/java/com/axway/apim/user/UserAppTest.java
@@ -0,0 +1,60 @@
+package com.axway.apim.user;
+
+import com.axway.apim.WiremockWrapper;
+import com.axway.apim.lib.utils.Utils;
+import com.axway.apim.users.UserApp;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+public class UserAppTest extends WiremockWrapper {
+
+    @BeforeClass
+    public void init() {
+        initWiremock();
+    }
+
+    @AfterClass
+    public void stop() {
+        close();
+    }
+    @Test
+    public void exportUsers(){
+        String[] args = {"-h", "localhost", "-loginName", "usera"};
+        int returnCode = UserApp.export(args);
+        Assert.assertEquals(returnCode, 0);
+    }
+
+    @Test
+    public void importUsers(){
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        String userFile = classLoader.getResource("com/axway/apim/users/userImport/user.json").getFile();
+        String[] args = {"-h", "localhost", "-c", userFile};
+        int returnCode = UserApp.importUsers(args);
+        Assert.assertEquals(returnCode, 0);
+    }
+
+    @Test
+    public void deleteUsers(){
+        String[] args = {"-h", "localhost", "-loginName", "usera"};
+        String input = "Y";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        int returnCode = UserApp.delete(args);
+        Assert.assertEquals(returnCode, 0);
+    }
+
+    @Test
+    public void changePassword(){
+        String[] args = {"-h", "localhost", "-loginName", "usera", "-newpassword", Utils.getEncryptedPassword()};
+        String input = "Y";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        int returnCode = UserApp.changePassword(args);
+        Assert.assertEquals(returnCode, 0);
+    }
+}
