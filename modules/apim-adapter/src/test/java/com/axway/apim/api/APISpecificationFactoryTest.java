72f5509261caf21faa1ded847b3d7fc22a2e1784
Rathnapandi <rathnapandi.n@gmail.com>
2023-01-19 22:07:36 -0700

    - Add junit

--- modules/apim-adapter/src/test/java/com/axway/apim/api/APISpecificationFactoryTest.java
+++ modules/apim-adapter/src/test/java/com/axway/apim/api/APISpecificationFactoryTest.java
@@ -14,6 +14,8 @@ public class APISpecificationFactoryTest {
         String specDirPath = classLoader.getResource("com/axway/apim/adapter/spec").getFile();
         APISpecification apiSpecification = APISpecificationFactory.getAPISpecification("openapi.json", specDirPath, "petstore");
         Assert.assertEquals(APISpecification.APISpecType.valueOf("OPEN_API_30"), apiSpecification.getAPIDefinitionType());
+        Assert.assertNotNull(apiSpecification.getDescription());
+        Assert.assertNotNull(apiSpecification.getApiSpecificationContent());
     }
 
     @Test
@@ -22,6 +24,8 @@ public class APISpecificationFactoryTest {
         String specDirPath = classLoader.getResource("com/axway/apim/adapter/spec").getFile();
         APISpecification apiSpecification = APISpecificationFactory.getAPISpecification("airports_swagger_20.json", specDirPath, "petstore");
         Assert.assertEquals(APISpecification.APISpecType.valueOf("SWAGGER_API_20"), apiSpecification.getAPIDefinitionType());
+        Assert.assertNotNull(apiSpecification.getDescription());
+        Assert.assertNotNull(apiSpecification.getApiSpecificationContent());
     }
 
     @Test
@@ -30,6 +34,8 @@ public class APISpecificationFactoryTest {
         String specDirPath = classLoader.getResource("com/axway/apim/adapter/spec").getFile();
         APISpecification apiSpecification = APISpecificationFactory.getAPISpecification("sample-accounts-api.wadl", specDirPath, "petstore");
         Assert.assertEquals(APISpecification.APISpecType.valueOf("WADL_API"), apiSpecification.getAPIDefinitionType());
+        Assert.assertNotNull(apiSpecification.getApiSpecificationContent());
+
     }
 
     @Test
@@ -38,6 +44,8 @@ public class APISpecificationFactoryTest {
         String specDirPath = classLoader.getResource("com/axway/apim/adapter/spec").getFile();
         APISpecification apiSpecification = APISpecificationFactory.getAPISpecification("sample.wsdl", specDirPath, "petstore");
         Assert.assertEquals(APISpecification.APISpecType.valueOf("WSDL_API"), apiSpecification.getAPIDefinitionType());
+        Assert.assertNotNull(apiSpecification.getApiSpecificationContent());
+
     }
 
 
@@ -47,6 +55,8 @@ public class APISpecificationFactoryTest {
         String specDirPath = classLoader.getResource("com/axway/apim/adapter/spec").getFile();
         APISpecification apiSpecification = APISpecificationFactory.getAPISpecification("ODataV2NorthWindMetadata.xml", specDirPath, "petstore");
         Assert.assertEquals(APISpecification.APISpecType.valueOf("ODATA_V2"), apiSpecification.getAPIDefinitionType());
+        Assert.assertNotNull(apiSpecification.getApiSpecificationContent());
+
     }
 
     @Test(expectedExceptions = AppException.class)
@@ -63,6 +73,8 @@ public class APISpecificationFactoryTest {
         String specDirPath = classLoader.getResource("com/axway/apim/adapter/spec").getFile();
         APISpecification apiSpecification = APISpecificationFactory.getAPISpecification("ODataV4TrippinServiceMetadata.xml", specDirPath, "petstore");
         Assert.assertEquals(APISpecification.APISpecType.valueOf("ODATA_V4"), apiSpecification.getAPIDefinitionType());
+        Assert.assertNotNull(apiSpecification.getApiSpecificationContent());
+
     }
 
     @Test
@@ -71,6 +83,8 @@ public class APISpecificationFactoryTest {
         String specDirPath = classLoader.getResource("com/axway/apim/adapter/spec").getFile();
         APISpecification apiSpecification = APISpecificationFactory.getAPISpecification("swagger12.json", specDirPath, "petstore");
         Assert.assertEquals(APISpecification.APISpecType.valueOf("SWAGGER_API_1x"), apiSpecification.getAPIDefinitionType());
+        Assert.assertNotNull(apiSpecification.getApiSpecificationContent());
+
     }
 
     @Test
@@ -79,6 +93,8 @@ public class APISpecificationFactoryTest {
         String specDirPath = classLoader.getResource("com/axway/apim/adapter/spec").getFile();
         APISpecification apiSpecification = APISpecificationFactory.getAPISpecification("swagger11.json", specDirPath, "petstore");
         Assert.assertEquals(APISpecification.APISpecType.valueOf("SWAGGER_API_1x"), apiSpecification.getAPIDefinitionType());
+        Assert.assertNotNull(apiSpecification.getApiSpecificationContent());
+
     }
 
     @Test(expectedExceptions = AppException.class)
