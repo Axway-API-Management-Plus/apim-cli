ef887ad4e2ed449ebc8501b096d1ac5ce4c61669
Rathnapandi <rathnapandi.n@gmail.com>
2023-01-19 21:54:48 -0700

    - Add junit

---
+++ modules/apim-adapter/src/test/java/com/axway/apim/api/APISpecificationFactoryTest.java
@@ -0,0 +1,93 @@
+package com.axway.apim.api;
+
+import com.axway.apim.api.apiSpecification.APISpecification;
+import com.axway.apim.api.apiSpecification.APISpecificationFactory;
+import com.axway.apim.lib.errorHandling.AppException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class APISpecificationFactoryTest {
+
+    @Test
+    public void getAPISpecificationOpenApi() throws AppException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        String specDirPath = classLoader.getResource("com/axway/apim/adapter/spec").getFile();
+        APISpecification apiSpecification = APISpecificationFactory.getAPISpecification("openapi.json", specDirPath, "petstore");
+        Assert.assertEquals(APISpecification.APISpecType.valueOf("OPEN_API_30"), apiSpecification.getAPIDefinitionType());
+    }
+
+    @Test
+    public void getAPISpecificationSwagger2() throws AppException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        String specDirPath = classLoader.getResource("com/axway/apim/adapter/spec").getFile();
+        APISpecification apiSpecification = APISpecificationFactory.getAPISpecification("airports_swagger_20.json", specDirPath, "petstore");
+        Assert.assertEquals(APISpecification.APISpecType.valueOf("SWAGGER_API_20"), apiSpecification.getAPIDefinitionType());
+    }
+
+    @Test
+    public void getAPISpecificationWADL() throws AppException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        String specDirPath = classLoader.getResource("com/axway/apim/adapter/spec").getFile();
+        APISpecification apiSpecification = APISpecificationFactory.getAPISpecification("sample-accounts-api.wadl", specDirPath, "petstore");
+        Assert.assertEquals(APISpecification.APISpecType.valueOf("WADL_API"), apiSpecification.getAPIDefinitionType());
+    }
+
+    @Test
+    public void getAPISpecificationWSDL() throws AppException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        String specDirPath = classLoader.getResource("com/axway/apim/adapter/spec").getFile();
+        APISpecification apiSpecification = APISpecificationFactory.getAPISpecification("sample.wsdl", specDirPath, "petstore");
+        Assert.assertEquals(APISpecification.APISpecType.valueOf("WSDL_API"), apiSpecification.getAPIDefinitionType());
+    }
+
+
+    @Test
+    public void getAPISpecificationOdataV2() throws AppException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        String specDirPath = classLoader.getResource("com/axway/apim/adapter/spec").getFile();
+        APISpecification apiSpecification = APISpecificationFactory.getAPISpecification("ODataV2NorthWindMetadata.xml", specDirPath, "petstore");
+        Assert.assertEquals(APISpecification.APISpecType.valueOf("ODATA_V2"), apiSpecification.getAPIDefinitionType());
+    }
+
+    @Test(expectedExceptions = AppException.class)
+    public void getAPISpecificationOdataV3() throws AppException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        String specDirPath = classLoader.getResource("com/axway/apim/adapter/spec").getFile();
+        APISpecification apiSpecification = APISpecificationFactory.getAPISpecification("ODataV3ODataDemoMetadata.xml", specDirPath, "petstore");
+        Assert.assertEquals(APISpecification.APISpecType.valueOf("ODATA_V3"), apiSpecification.getAPIDefinitionType());
+    }
+
+    @Test
+    public void getAPISpecificationOdataV4() throws AppException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        String specDirPath = classLoader.getResource("com/axway/apim/adapter/spec").getFile();
+        APISpecification apiSpecification = APISpecificationFactory.getAPISpecification("ODataV4TrippinServiceMetadata.xml", specDirPath, "petstore");
+        Assert.assertEquals(APISpecification.APISpecType.valueOf("ODATA_V4"), apiSpecification.getAPIDefinitionType());
+    }
+
+    @Test
+    public void getAPISpecificationSwagger12() throws AppException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        String specDirPath = classLoader.getResource("com/axway/apim/adapter/spec").getFile();
+        APISpecification apiSpecification = APISpecificationFactory.getAPISpecification("swagger12.json", specDirPath, "petstore");
+        Assert.assertEquals(APISpecification.APISpecType.valueOf("SWAGGER_API_1x"), apiSpecification.getAPIDefinitionType());
+    }
+
+    @Test
+    public void getAPISpecificationSwagger11() throws AppException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        String specDirPath = classLoader.getResource("com/axway/apim/adapter/spec").getFile();
+        APISpecification apiSpecification = APISpecificationFactory.getAPISpecification("swagger11.json", specDirPath, "petstore");
+        Assert.assertEquals(APISpecification.APISpecType.valueOf("SWAGGER_API_1x"), apiSpecification.getAPIDefinitionType());
+    }
+
+    @Test(expectedExceptions = AppException.class)
+    public void getAPISpecificationUnknown() throws AppException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        String specDirPath = classLoader.getResource("com/axway/apim/adapter/spec").getFile();
+        APISpecification apiSpecification = APISpecificationFactory.getAPISpecification("unknown.txt", specDirPath, "petstore");
+        Assert.assertEquals(APISpecification.APISpecType.valueOf("UNKNOWN"), apiSpecification.getAPIDefinitionType());
+    }
+
+
+}
