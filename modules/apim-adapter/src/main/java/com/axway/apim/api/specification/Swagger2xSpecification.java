6fe32e6bade40d5097f8023838a66c619d8e95e8
gcornacchia <gcornacchia@imolainformatica.it>
2023-07-12 18:23:59 +0200

    added test in case scheme is https but backendbasepath is http

--- modules/apim-adapter/src/main/java/com/axway/apim/api/specification/Swagger2xSpecification.java
+++ modules/apim-adapter/src/main/java/com/axway/apim/api/specification/Swagger2xSpecification.java
@@ -93,17 +93,20 @@ public class Swagger2xSpecification extends APISpecification {
                 }
                 URL url = new URL(backendBasePath);
                 String port = url.getPort() == -1 ? ":" + url.getDefaultPort() : ":" + url.getPort();
+                Boolean adjustedHost = false;
                 if (port.equals(":443") || port.equals(":80")) port = "";
                 if (swagger.get("host") == null) {
                     LOG.debug("Adding new host {}{} to Swagger-File based on backendBasePath: {}", url.getHost(), port, backendBasePath);
                     ((ObjectNode) swagger).put("host", url.getHost() + port);
                     LOG.info("Used the backendBasePath: {} to adjust host the API-Specification.", backendBasePath);
+                    adjustedHost = true;
                 }else {
                     if(swagger.get("host").asText().equals(url.getHost()+port)) {
                         LOG.debug("Swagger Host: '{}' already matches backendBasePath: '{}'. Nothing to do.",swagger.get("host").asText(),backendBasePath);
                     } else if (CoreParameters.getInstance().isOverrideSpecBasePath()){
                         LOG.debug("Replacing existing host: '"+swagger.get("host").asText()+"' in Swagger-File to '"+url.getHost()+port+"' based on configured backendBasePath: '"+backendBasePath+"'");
                         ((ObjectNode)swagger).put("host", url.getHost()+port);
+                        adjustedHost = true;
                     }
                 }
                 //what if the backendBasePath is http?
@@ -113,7 +116,7 @@ public class Swagger2xSpecification extends APISpecification {
                     LOG.debug("Adding protocol: {} to Swagger-Definition", url.getProtocol());
                     ((ObjectNode) swagger).set("schemes", newSchemes);
                 } else {
-                    if (CoreParameters.getInstance().isOverrideSpecBasePath()) {
+                    if (CoreParameters.getInstance().isOverrideSpecBasePath() || adjustedHost) {
                         //I may have a situation where a backendbasepath in http must overwrite host but in swagger file it's declared a scheme in https which is not coherent
                         ArrayNode schemes = (ArrayNode) swagger.get("schemes");
                         schemes.removeAll();
