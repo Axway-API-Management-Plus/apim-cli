554117cc97626c55ea99c3967e09194c17724738
rathnapandi <rnatarajan@axway.com>
2026-07-15 10:46:27 -0700

    support #556

--- modules/apis/src/test/java/com/axway/apim/APIImportAppTest.java
+++ modules/apis/src/test/java/com/axway/apim/APIImportAppTest.java
@@ -8,6 +8,9 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.axway.apim.lib.error.AppException;
+import com.axway.apim.lib.error.ErrorCode;
+
 public class APIImportAppTest extends WiremockWrapper {
 
     @BeforeClass
@@ -58,5 +61,60 @@ public class APIImportAppTest extends WiremockWrapper {
         API desiredAPI = configAdapter.getDesiredAPI();
         Assert.assertEquals("",desiredAPI.getAuthenticationProfiles().get(0).getParameters().get("password"));
 
+    }
+
+    @Test
+    public void safeUpdateAllowsSameName() throws AppException {
+        APIImportApp app = new APIImportApp();
+        APIImportParams params = new APIImportParams();
+        params.setSafeUpdate(true);
+
+        API actualApi = new API();
+        actualApi.setId("actual-id");
+        actualApi.setName("Order API");
+
+        API desiredApi = new API();
+        desiredApi.setId("desired-id");
+        desiredApi.setName("Order API");
+        desiredApi.setPath("/api/orders/v1");
+
+        app.validateSafeUpdate(actualApi, desiredApi, params);
+    }
+
+    @Test
+    public void safeUpdateAllowsNameMismatchWhenApiIdMatches() throws AppException {
+        APIImportApp app = new APIImportApp();
+        APIImportParams params = new APIImportParams();
+        params.setSafeUpdate(true);
+
+        API actualApi = new API();
+        actualApi.setId("same-id");
+        actualApi.setName("Order API");
+
+        API desiredApi = new API();
+        desiredApi.setId("same-id");
+        desiredApi.setName("Inventory API");
+        desiredApi.setPath("/api/orders/v1");
+
+        app.validateSafeUpdate(actualApi, desiredApi, params);
+    }
+
+    @Test
+    public void safeUpdateBlocksNameMismatchForSamePath() {
+        APIImportApp app = new APIImportApp();
+        APIImportParams params = new APIImportParams();
+        params.setSafeUpdate(true);
+
+        API actualApi = new API();
+        actualApi.setId("actual-id");
+        actualApi.setName("Order API");
+
+        API desiredApi = new API();
+        desiredApi.setId("different-id");
+        desiredApi.setName("Inventory API");
+        desiredApi.setPath("/api/orders/v1");
+
+        AppException exception = Assert.expectThrows(AppException.class, () -> app.validateSafeUpdate(actualApi, desiredApi, params));
+        Assert.assertEquals(exception.getError(), ErrorCode.API_PATH_IN_USE_BY_DIFFERENT_API);
     }
 }
